b1e66a3021dac50a9aaac2e147e82854924d922a
Cyril Lamotte <lamotte.cyril@gmail.com>
2017-09-30 13:23:22 +0200

    Nouvelle branche

---
+++ dist/assets/js/app.config.js
@@ -0,0 +1,15 @@
+/**
+ * @file App initialisation & domready event.
+ *
+ * Only the "app" variable is exposed to window's context.
+ */
+(function($) {
+
+"use strict";
+
+window.app = {
+  dbName: 'score',
+  dbVersion: 3
+};
+
+})();
---
+++ dist/assets/js/app.js
@@ -0,0 +1,71 @@
+(function($) {
+
+"use strict";
+
+app.init = function() {
+
+  app.checkCompatibility();
+
+  // For dev.
+  //app.deleteDb();
+
+  // Prod.
+  app.createDB();
+
+};
+
+app.init();
+
+
+$(function() {
+
+
+  // Set current player.
+  $('body').on('click', '.score__row', function() {
+    app.currentPlayerId = $(this).attr('data-score-player-id');
+  });
+
+  $('body')
+    .on('click', 'button[data-init="add-score"]', function(event) {
+
+      // Show score form.
+      $('#modal-add').show();
+      app.getPlayerId($(this));
+
+  }).on('click', 'button[data-init="close-modal"]', function(event) {
+
+    // Hide score form.
+    $('#modal-add').hide();
+
+  }).on('click', 'button[data-score-add-to]', function(event) {
+
+    // Add value from select.
+    var addedValue = parseInt($('#add').val());
+    app.updateScore(addedValue);
+
+  }).on('click', 'button[data-init="add-score-fixed"]', function(event) {
+    event.preventDefault();
+
+    var value = parseInt($(this).attr('data-score-value'));
+    app.getPlayerId($(this));
+    app.updateScore(value);
+
+  }).on('click', 'button[data-init="init-score"]', function(event) {
+    event.preventDefault();
+
+    app.getPlayerId($(this));
+    app.updateScore(0);
+
+  });
+
+
+  $('body').on('keyup', '.score__name', function(event) {
+    app.getPlayerId($(this));
+    app.updatePlayerName($(this).text());
+  })
+
+
+}); // /ready
+
+
+})(jQuery);
